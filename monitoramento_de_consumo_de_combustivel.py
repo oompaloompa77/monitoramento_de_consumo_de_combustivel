c206677adb5354b71b24f839811ab28515835e77
oompaloompa77 <paula.mariana2019.pmom@gmail.com>
2025-05-20 16:58:00 -0300

    alterações basicas

---
+++ monitoramento_de_consumo_de_combustivel.py
@@ -0,0 +1,11 @@
+#Criar um programa que faça o monitoramento de combustivel
+#Perguntar a pessoa a distância percorrida (em km)
+#Quantidade de combustivel gasto (em litros)
+#O programa deve calcular o consumo_medio:
+#< 8 = Alto consumo
+#8 – 12 = Consumo moderado
+#> 12 = Econômico
+#Dizer o resultado final com: alto consumo, cxonsumo moderado, econômico
+
+input(consumo_medio = distancia / litros)
+print('Digite a distância percorrida (em km):' {Perguntar a pessoa a distância percorrida (em km)})
